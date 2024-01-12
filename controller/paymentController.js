2ccc57c512776cfebb0c08e9b90f11fe9a9fc220
hridoy281810 <hridoy01724281810@gmail.com>
2024-01-12 19:46:51 +0600

    create link

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -1,6 +1,35 @@
+const axios = require('axios')
+const globals = require('node-global-storage')
+const { v4: uuidv4 } = require('uuid');
 class paymentController {
+    bkash_headers = async ()=>{
+        return {
+                "Content-Type":	"application/json",
+                Accept: "application/json",
+                authorization: globals.get('id_token'),
+                'x-app-key': process.env.bkash_api_key
+            
+        }
+    }
     payment_create = async(req,res)=> {
-        console.log(req.body)
+       const {amount} = req.body
+       try {
+        const {data} = await axios.post(process.env.bkash_create_payment_url,{
+              mode: '0011',
+              payerReference: " ",
+              callbackURL:  'http://localhost:5000/bkash/payment/callback',
+              amount: amount,
+              currency: "BDT",
+              intent: "sale",
+              merchantInvoiceNumber: 'Inv' + uuidv4().substring(0,5)
+        },{
+       headers: await this.bkash_headers()
+        })
+        console.log(data)
+        // return res.status(200).json({bkashURl:data})
+       } catch (error) {
+        return res.status(401).json({error: error.message})
+       }
     }
 }
 module.exports = new paymentController();
--- middleware/middleware.js
+++ middleware/middleware.js
@@ -5,9 +5,23 @@ class middleware {
     bkash_auth = async (req,res,next)=>{
    globals.unset('id_token')
    try {
-    const {data} =  await axios.post()
+    const {data} =  await axios.post(process.env.bkash_grant_token_url,{
+        app_key:process.env.bkash_api_key,
+        app_secret:process.env.bkash_secret_key,
+    },{
+        headers: {
+            "Content-Type":	"application/json",
+            Accept: "application/json",
+            username: process.env.bkash_username,
+            password: process.env.bkash_password
+        }
+    })
+    globals.set('id_token',data.id_token,{protected: true})
+    next()
+    // console.log(data)
    } catch (error) {
-    
+    console.log(error.message)
+    return res.status(401).json({error: error.message})
    }
     }
 }
